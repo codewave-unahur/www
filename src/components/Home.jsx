968f50b8903d3dcdadfeef0da629081c23547b11
adrian yaniri <agyaniri@gmail.com>
2023-11-08 09:09:16 -0300

    ser remueve el componente Feed del Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,6 @@ import ContactForm from "./Home/ContactForm"
 import SliderCarousel from "./Home/SliderCarousel";
 import Servicios from "./Home/Servicios"
 import Portfolio from "./Home/Portfolio"
-import Feed from "./Feed";
 
 export default function Home() {
   return (
@@ -17,8 +16,6 @@ export default function Home() {
         <Servicios/>
         <Portfolio/>
         <ContactForm/>
-            <h1>Feed de Instagram</h1>
-           <Feed/>
         <Footer/>
       </Box>
     </>
